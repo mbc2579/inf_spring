bd36dc0937369b480801176057c910464a708ebe
mbc2579 <mbc2579@naver.com>
2024-06-03 18:07:52 +0900

    회원 웹 기능 - 등록

--- src/main/java/hello/inf_spring/controller/MemberController.java
+++ src/main/java/hello/inf_spring/controller/MemberController.java
@@ -1,8 +1,11 @@
 package hello.inf_spring.controller;
 
+import hello.inf_spring.domain.Member;
 import hello.inf_spring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MemberController {
@@ -15,5 +18,20 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
