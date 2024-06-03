0f3e58b7f92da66d862d7b49d8cc7dafc4512e05
mbc2579 <mbc2579@naver.com>
2024-06-03 21:00:46 +0900

    순수 JDBC

--- src/main/java/hello/inf_spring/SpringConfig.java
+++ src/main/java/hello/inf_spring/SpringConfig.java
@@ -1,13 +1,25 @@
 package hello.inf_spring;
 
+import hello.inf_spring.repository.JdbcMemberRepository;
 import hello.inf_spring.repository.MemberRepository;
 import hello.inf_spring.repository.MemoryMemberRepository;
 import hello.inf_spring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
