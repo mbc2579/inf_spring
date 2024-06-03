d4ea9415b9c7a3475ce4f0fc598fb5d3bd44057d
mbc2579 <mbc2579@naver.com>
2024-06-03 21:06:24 +0900

    gitignore 재설정 후 커밋

---
+++ src/main/java/hello/inf_spring/InfSpringApplication.java
@@ -0,0 +1,13 @@
+package hello.inf_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InfSpringApplication.class, args);
+	}
+
+}
