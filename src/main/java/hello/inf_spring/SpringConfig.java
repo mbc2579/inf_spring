835b5d95a9fc62e4688ac01bf210ef467e56af5a
mbc2579 <mbc2579@naver.com>
2024-06-03 20:37:05 +0900

    위치(경로)변경

--- src/main/java/hello/inf_spring/SpringConfig.java
+++ src/main/java/hello/inf_spring/SpringConfig.java
@@ -1,7 +1,8 @@
-package hello.inf_spring.service;
+package hello.inf_spring;
 
 import hello.inf_spring.repository.MemberRepository;
 import hello.inf_spring.repository.MemoryMemberRepository;
+import hello.inf_spring.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
