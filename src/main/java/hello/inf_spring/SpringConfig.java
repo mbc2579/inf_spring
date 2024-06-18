851fb8ac4fa9eb30516cf673625557d8e0433a64
mbc2579 <mbc2579@naver.com>
2024-06-18 19:27:06 +0900

    JPA

--- src/main/java/hello/inf_spring/SpringConfig.java
+++ src/main/java/hello/inf_spring/SpringConfig.java
@@ -1,10 +1,8 @@
 package hello.inf_spring;
 
-import hello.inf_spring.repository.JdbcMemberRepository;
-import hello.inf_spring.repository.JdbcTemplateMemberRepository;
-import hello.inf_spring.repository.MemberRepository;
-import hello.inf_spring.repository.MemoryMemberRepository;
+import hello.inf_spring.repository.*;
 import hello.inf_spring.service.MemberService;
+import jakarta.persistence.EntityManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.batch.BatchProperties;
 import org.springframework.context.annotation.Bean;
@@ -17,11 +15,10 @@ import javax.sql.DataSource;
 @Configuration
 public class SpringConfig {
 
-    private DataSource dataSource;
+    private EntityManager em;
 
-    @Autowired
-    public SpringConfig(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public SpringConfig(EntityManager em) {
+        this.em = em;
     }
 
     @Bean
@@ -33,6 +30,7 @@ public class SpringConfig {
     public MemberRepository memberRepository() {
 //        return new MemoryMemberRepository();
 //        return new JdbcMemberRepository(dataSource);
-        return new JdbcTemplateMemberRepository(dataSource);
+//        return new JdbcTemplateMemberRepository(dataSource);
+        return new JpaMemberRepository(em);
     }
 }
